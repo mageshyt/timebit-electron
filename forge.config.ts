3fa955b03892abbf3736b8c0310a9496ae0e1ff0
mageshyt <mageshytdev@gmail.com>
2026-05-23 16:46:46 +0530

    chore: rename application to TimeBit and add custom app icons

--- forge.config.ts
+++ forge.config.ts
@@ -1,5 +1,6 @@
 import { FuseV1Options, FuseVersion } from "@electron/fuses";
 import { MakerDeb } from "@electron-forge/maker-deb";
+import { MakerDMG } from "@electron-forge/maker-dmg";
 import { MakerRpm } from "@electron-forge/maker-rpm";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
@@ -10,11 +11,13 @@ import type { ForgeConfig } from "@electron-forge/shared-types";
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    icon: './images/icon',
   },
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({}),
     new MakerZIP({}, ["darwin"]),
+    new MakerDMG({}, ["darwin"]),
     new MakerRpm({}),
     new MakerDeb({}),
   ],
